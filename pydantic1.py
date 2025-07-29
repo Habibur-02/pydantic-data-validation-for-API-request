10c408eb34afc832d4dc624e577435d6f807e579
Habibur <hraasif02@gmail.com>
2025-07-29 17:06:44 +0600

    ff

--- pydantic1.py
+++ pydantic1.py
@@ -4,7 +4,7 @@ class patients(BaseModel):
     name: str
     age: int
     weight: float
-    married: bool
+    married: bool = False
     allergies: Optional[list[str]]=None
     contact: Dict[str,str]
 
@@ -13,13 +13,14 @@ class patients(BaseModel):
 def print_patient_info(patient: patients):
     print(patient.age)
     print(patient.name)
+    print(patient.married)
     print(patient.allergies)
 
 patient_info={'name':'Aasif',
               'age': '25',
               'weight': '33.33',
               'married': True,
-            #   'allergies':['pollen','dust'],
+              'allergies':['pollen','dust'],
               'contact':{'email':'abc@gmail.com',
                          'phone':'+8801748150901'}}
 
