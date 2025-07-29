2c094c05347655f215f250bd335f2e7c037b52f8
Habibur <hraasif02@gmail.com>
2025-07-29 16:03:25 +0600

    commit 2nd

--- pydantic1.py
+++ pydantic1.py
@@ -4,10 +4,17 @@ class patients(BaseModel):
     name: str
     age: int
 
+def print_patient_info(patient: patients):
+    print(patient.age)
+    print(patient.name)
+
 patient_info={'name':'Aasif',
               'age': 25}
 
 patient=patients(**patient_info)
 
+print_patient_info(patient)
+
 print(patient.age)
 print(patient.name)
+
